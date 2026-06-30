96d708eb1c1d5df824d25e563fea72022cf69d8d
GGaboBD <007007gabo7u7@gmail.com>
2026-06-30 16:25:55 -0600

    Control 2

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # Repositorio temporal de datos (Corregido sin espacios invisibles)
 libros = {
-    101: {"id": 101, "titulo": "Clean Code", "autor": "Robert C. Martin", "Disponible": True},
-    102: {"id": 102, "titulo": "Python Crash Course", "autor": "Eric Matthew", "Disponible": True},
-    103: {"id": 103, "titulo": "Architecture Patterns", "autor": "GoF", "Disponible": False}
+    101: {"id": 101, "titulo": "Clean Code", "autor": "Robert C. Martin", "disponible": True},
+    102: {"id": 102, "titulo": "Python Crash Course", "autor": "Eric Matthew", "disponible": True},
+    103: {"id": 103, "titulo": "Architecture Patterns", "autor": "GoF", "disponible": False}
 }
 
 @app.get("/")  # 127.0.0.1:5000
@@ -23,5 +23,37 @@ def inicio():
         ] 
     })
 
+@app.get("/libros")
+def mostrar_libros():
+    return jsonify(list(libros.values()))
+
+@app.get("/libros/<int:id>")
+def obtener_libro(id):
+    libro = libros.get(id)
+
+    if libro:
+        return jsonify(libro)
+    return jsonify({"error": "libro no encontrado"}),404
+
+@app.post("/libros")
+def agregar_libro():
+    datos = request.get_json()
+
+    if not datos:
+        return jsonify({"error": "Debe enviar informacion"})
+    if "titulo" not in datos or "autor" not in datos or "disponible" not in datos:
+        return jsonify({"error": "Los campos son requeridos"}), 400
+    
+    nuevo_id = max(libros.keys())+1
+
+    libros[nuevo_id] = {
+        "id": nuevo_id,
+        "titulo": datos["titulo"],
+        "autor": datos["autor"],
+        "disponible": datos["disponible"]
+    }
+
+    return jsonify(libros[nuevo_id]),201
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
